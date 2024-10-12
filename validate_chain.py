e9114b9071f3bcf54025de3f19fe117b2bc9bf00
giteed <tradeeed@gmail.com>
2024-10-12 08:55:46 +0300

    .

--- validate_chain.py
+++ validate_chain.py
@@ -1,23 +1,17 @@
 import os
-from validate_block import validate_block
+from block import validate_block
 
 def validate_all_blocks():
     """Функция для валидации всех блоков."""
-    
     files = [f for f in os.listdir('blockchain') if f.endswith('.txt')]
-    files.sort()  # Убедимся, что блоки проверяются по порядку
-
     all_valid = True
-    previous_block_hash = None
 
     for file in files:
-        # Проверяем каждый блок, передавая хеш предыдущего блока для проверки
-        if not validate_block(os.path.join('blockchain', file), previous_block_hash):
+        if not validate_block(os.path.join('blockchain', file)):
+            print(f"Блок {file} не валиден.")
             all_valid = False
         else:
-            # Считываем текущий хеш как предыдущий для следующей итерации
-            with open(os.path.join('blockchain', file), 'r') as f:
-                previous_block_hash = f.readlines()[1].strip().split(": ")[1]
+            print(f"Блок {file} валиден.")
 
     return all_valid
 
